f6eda9a020376e91fc95cbdbb04279373e95fca6
marcmacias96 <mamacias3@espe.edu.ec>
2020-02-13 12:19:17 -0500

    Add apollo

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
+import { createProvider } from './vue-apollo'
 
 /**
  * If you don't want to use mock-server
@@ -39,5 +40,6 @@ new Vue({
   el: '#app',
   router,
   store,
+  apolloProvider: createProvider(),
   render: h => h(App)
 })

15a96494c4334c2fabae7c704da5fc638a8eb6d9
marcmacias96 <mamacias3@espe.edu.ec>
2020-02-20 10:41:48 -0500

    pull

--- src/router/index.js
+++ src/router/index.js
@@ -74,6 +74,12 @@ export const constantRoutes = [
         name: 'Tree',
         component: () => import('@/views/tree/index'),
         meta: { title: 'Tree', icon: 'tree' }
+      },
+      {
+        path: 'complexTable',
+        name: 'ComplexTable',
+        component: () => import('@/views/table/complexTable'),
+        meta: { title: 'Reporte de tramites', icon: 'table' }
       }
     ]
   },

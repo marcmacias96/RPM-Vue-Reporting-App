8d08048395532525dadc0197302e8de81cb4c114
marcmacias96 <mamacias3@espe.edu.ec>
2020-10-16 09:38:47 -0500

    fix not found 2

--- src/main.js
+++ src/main.js
@@ -37,7 +37,7 @@ Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
-Vue.config.productionTip = true
+Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
